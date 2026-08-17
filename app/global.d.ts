1f6606948fc7bd95e5648556de64bac6eaa3b776
Masayidalfa <masayidalfarizqi@gmail.com>
2026-08-17 14:09:00 +0700

    fix bugs

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,23 +1,31 @@
-export { };
+import type { Object3DNode } from '@react-three/fiber'
+import type { BufferGeometry, Material } from 'three'
+
+export {};
 
 declare module '*.glb';
 declare module '*.png';
 
 declare module 'meshline' {
-  export const MeshLineGeometry: any;
-  export const MeshLineMaterial: any;
+  export class MeshLineGeometry extends BufferGeometry {
+    setPoints(points: number[] | Float32Array, callback?: (p: number) => number): void
+  }
+  export class MeshLineMaterial extends Material {
+    constructor(parameters?: Record<string, unknown>)
+  }
 }
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      meshLineGeometry: any;
-      meshLineMaterial: any;
+      meshLineGeometry: Object3DNode
+        import('meshline').MeshLineGeometry,
+        typeof import('meshline').MeshLineGeometry
+      >
+      meshLineMaterial: Object3DNode
+        import('meshline').MeshLineMaterial,
+        typeof import('meshline').MeshLineMaterial
+      >
     }
   }
 }
-
-- src/vite-env.d.ts
-/// <reference types="vite/client" />
-declare module '*.glb';
-declare module '*.png';
